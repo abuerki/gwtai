2cfec36782e7ff69611b1a6274e76e3a5400df30
michael.krog <michael.krog@b92bad58-d051-0410-916e-712eaba9a294>
2011-02-04 06:54:08 +0000

    Cleanup.

--- branches/branch_0.4.1/gwtaidemo/src/main/java/com/google/gwt/gwtai/demo/client/CallbackApplet.java
+++ branches/branch_0.4.1/gwtaidemo/src/main/java/com/google/gwt/gwtai/demo/client/CallbackApplet.java
@@ -1,7 +1,6 @@
 package com.google.gwt.gwtai.demo.client;
 
 import com.google.gwt.gwtai.applet.client.Applet;
-import com.google.gwt.gwtai.applet.client.Archive;
 import com.google.gwt.gwtai.applet.client.Height;
 import com.google.gwt.gwtai.applet.client.ImplementingClass;
 import com.google.gwt.gwtai.applet.client.Width;
@@ -9,7 +8,6 @@ import com.google.gwt.gwtai.applet.client.Width;
 @ImplementingClass(com.google.gwt.gwtai.demo.impl.CallbackAppletImpl.class)
 @Height("100")
 @Width("350")
-@Archive("com.google.gwt.gwtai.demo.GwtAI/GwtAI-Demo.jar")
 public interface CallbackApplet extends Applet {
 
 }
